f88d2546a080de563943dcc5a586e6c5ebb2e046
ShomerDaSilva <83773296+ShomerDaSilva@users.noreply.github.com>
2021-09-12 16:54:06 -0500

    practica 5

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,32 +1,24 @@
 import { Component } from '@angular/core';
+import datosUsuario from '../../feed.json';
+interface Usuario{
+  nombre: String;
+  seguidores: number;
+  siguiendo: number;
+  posts: number;
+  bio: String;
+  avatar: String;
+  publicaciones: Array<String>;
+}
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
-  followers="2M";
-  following="3"
-  posts= "12";
-  description="Hola soy Max y soy un perro feliz :D ";
-  avatar = 'Ruffles.png';
-  postsURLS = [
-    'p1.png',
-    'p2.png',
-    'p3.png',
-    'p4.png',
-    'p5.png',
-    'p6.png',
-    'p7.png',
-    'p8.png',
-    'p9.png',
-    'p10.png',
-    'p11.png',
-    'p12.png',
-  ];
 
-  esUsuario = false;
+export class Tab3Page {
+
+  usuario: Usuario = datosUsuario.usuario;
 
   constructor() {}
 
