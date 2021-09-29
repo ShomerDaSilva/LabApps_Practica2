eb01085ad2b0f9a5efaea4e6adce61c1292f518b
ShomerDaSilva <83773296+ShomerDaSilva@users.noreply.github.com>
2021-09-29 11:52:08 -0500

    practica7

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,17 @@
-import { Component, Output } from '@angular/core';
+import { Component, Output, Query } from '@angular/core';
+//import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import {Database, objectVal, ref} from '@angular/fire/database';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
 import feed  from '../../feed.json';
 
 export interface Publicaciones{
   id: number;
   usuario: String;
-  imagenPost: String;
-  avatarUsuario: String;
+  //imagenPost: String;
+  //avatarUsuario: String;
   descripcionPost: String;
-  likes: number;
+  //likes: number;
 }
 @Component({
   selector: 'app-tab1',
@@ -16,7 +20,20 @@ export interface Publicaciones{
 })
 export class Tab1Page {
 
-  @Output() posts: Publicaciones[] = feed.feed;
-  constructor() {}
+  //@Output() posts: Publicaciones[] = feed.feed;
+  getDataBase(){
+    //this.bd.list('/');
+    
+  }
 
+  usuarios$: AngularFireList<any[]>//Observable<Item[];
+
+  constructor(private b_d: Database) {
+    const bd = ref(b_d, 'prueba');
+    this.usuarios$ = objectVal(bd).pipe(trace(b_d));
+  }
+  ngOnInit(){
+    this.getDataBase();
+  }
 }
+
